7d1d7011d6de146c82acff2db309f36860f4a12e
RAVI SHEKHAR TIWARI <tiwari11.rst@gmail.com>
2021-01-19 18:05:50 +0530

    Update astar.py
    Bugs resolved

--- Astar-Gui/astar.py
+++ Astar-Gui/astar.py
@@ -4,7 +4,7 @@ from queue import PriorityQueue
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorithm")
+pygame.display.set_caption("A* Path Finding Algorithm by RSTIWARI link: https://www.rstiwari.com")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
